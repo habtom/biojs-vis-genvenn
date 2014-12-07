35c4834442322cb042c4cd101b4388ed5c565a24
habtom <habtomk@gmail.com>
2014-12-08 00:30:45 +0100

    three intersecting circles

--- gvenn.js
+++ gvenn.js
@@ -1,56 +1,40 @@
 function myFunction() {	
 
-var svg = d3.select("#container2")
+var gvennStage = d3.select("#gstage")
 				.append("svg")
-				.attr("width",500)
-				.attr("height",500);
-var circles = svg.selectAll("circle")
-					 .data([
-					 	{ size: 100 }
-					 ]);
+				.attr("width",600)
+				.attr("height",420);
+var plotCircles = gvennStage.selectAll("circle")
+					 .data([{ size: 100 }]);
 
 	if(document.getElementById("title1").value.length != 0){
 
-
-
 	
-	circles.enter()
-		   .append("circle")
-		   .attr("r",90
-		   	)
-		   .attr("cx",100
-		   )
-		   .attr("cy",100
-		   )
-		   .attr("fill", 'blue')
+	plotCircles.enter().append("circle")
+		.style("fill", "#00bf00")
+		.style("fill-opacity", 0.5)
+		.attr("r", 110)
+		.attr("cx", 230)
+		.attr("cy", 306)
     }
 	if(document.getElementById("title2").value.length != 0){
-	// bind some data
 
-	circles.enter()
-		   .append("circle")
-		   .attr("r",90
-		   	)
-		   .attr("cx",200
-		   )
-		   .attr("cy",300
-		   )
-		   .attr("fill", 'green')
+	plotCircles.enter().append("circle")
+		.style("fill", "#007fff")
+		.style("fill-opacity", 0.5)
+		.attr("r", 110)
+		.attr("cx", 370)
+		.attr("cy", 306)
     }
     if(document.getElementById("title3").value.length != 0){
-	// bind some data
 
-	circles.enter()
-		   .append("circle")
-		   .attr("r",90
-		   	)
-		   .attr("cx",550
-		   )
-		   .attr("cy",500
-		   )
-		   .attr("fill", 'yellow')
+	plotCircles.enter().append("circle")
+		.style("fill", "#007fff")
+		.style("fill-opacity", 0.5)
+		.attr("r", 110)
+		.attr("cx", 300)
+		.attr("cy", 185)
     }
-	// draw some circles
   }
   
 
